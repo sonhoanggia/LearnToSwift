d93efa143b2d43c312d1f857ff3d58074ffa44e7
Dương Ngọc Sơn <duongson29111997@gmail.com>
2018-01-27 11:30:13 +0700

    Thêm các button refresh, back, next

--- demoWebView/demoWebView/ViewController.swift
+++ demoWebView/demoWebView/ViewController.swift
@@ -12,6 +12,43 @@ class ViewController: UIViewController {
     @IBOutlet weak var txtSearch: UITextField!
     @IBOutlet weak var wvContent: UIWebView!
     
+    @IBAction func btnReload(_ sender: Any) {
+        wvContent.reload()
+    }
+
+//    Hàm truy cập website
+    
+    func accessWeb(link:String) -> Void {
+        let url2:URL = URL(string: link)!
+        let rq:URLRequest = URLRequest(url: url2)
+        wvContent.loadRequest(rq)
+    }
+    
+//  Khai báo mảng chứa chuỗi lịch sử link đã truy cập
+    
+    var history:Array<String> = Array<String>()
+    var i:Int = 0
+    
+    @IBAction func btnNext(_ sender: Any) {
+        i += 1
+        if (i >= history.count) {
+            i = history.count - 1
+        } else {
+            accessWeb(link: history[i])
+            txtSearch.text = history[i]
+        }
+    }
+    
+    @IBAction func btnBack(_ sender: Any) {
+        i -= 1
+        if (i < 0) {
+            i = 0
+        } else {
+            accessWeb(link: history[i])
+            txtSearch.text = history[i]
+        }
+    }
+    
     @IBAction func btnSearch(_ sender: Any) {
         if let url = URL(string: txtSearch.text!) {
             var link:String = txtSearch.text!
@@ -21,9 +58,9 @@ class ViewController: UIViewController {
                 wvContent.loadRequest(rq)
             } else {
                 link = "https://\(link)"
-                let url2:URL = URL(string: link)!
-                let rq:URLRequest = URLRequest(url: url2)
-                wvContent.loadRequest(rq)
+                accessWeb(link: link)
+                history.insert(link, at: i)
+                i += 1
             }
         } else {
             print("Lỗi chưa nhập URL.")
@@ -34,7 +71,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         txtSearch.placeholder = "Nhập URL"
-        txtSearch.clearButtonMode = .whileEditing
     }
 
     override func didReceiveMemoryWarning() {

da292564adf9f3276c0df1eee2ddfda0818f94e1
Dương Ngọc Sơn <duongson29111997@gmail.com>
2018-01-30 22:36:59 +0700

    chuyển màn hình

---
+++ manHinh/manHinh/manHinh2ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  manHinh2ViewController.swift
+//  manHinh
+//
+//  Created by Duong Son on 2018/01/29.
+//  Copyright © 2018年 apple. All rights reserved.
+//
+
+import UIKit
+
+class manHinh2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

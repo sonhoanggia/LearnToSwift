3df6bf13e9b628709e670e77fb10d864dce517b6
Dương Ngọc Sơn <duongson29111997@gmail.com>
2018-02-05 22:07:46 +0700

    bắt sự kiện chọn dòng của picker

--- pickerViewImage/pickerViewImage/ViewController.swift
+++ pickerViewImage/pickerViewImage/ViewController.swift
@@ -10,12 +10,15 @@ import UIKit
 
 class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     @IBOutlet weak var pickerImage: UIPickerView!
+    @IBOutlet weak var imgSelected: UIImageView!
+    
     var arrImg:Array<UIImage> = [#imageLiteral(resourceName: "image1"), #imageLiteral(resourceName: "image2"), #imageLiteral(resourceName: "image3"), #imageLiteral(resourceName: "image4"), #imageLiteral(resourceName: "image5")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         pickerImage.delegate = self
         pickerImage.dataSource = self
+        pickerImage.selectRow(3, inComponent: 0, animated: true)
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,14 +35,18 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     
     func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
         let contentPicker:UIImageView = UIImageView()
-        contentPicker.frame = CGRect(x: 10, y: 10, width: 100, height: 100)
-        contentPicker.contentMode = .scaleAspectFit
+        contentPicker.frame = CGRect(x: 10, y: 10, width: 150, height: 80)
+        contentPicker.contentMode = .scaleToFill
         contentPicker.image = arrImg[row]
         return contentPicker
     }
     
     func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
         return 100
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        imgSelected.image = arrImg[row]
     }
 }
 
